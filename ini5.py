50681909d00d0b46c39fa6543e8a3a14b35a44e3
Luiz Ferreira <luizferreira91@gmail.com>
2020-06-24 21:12:43 -0300

    ini5 added

---
+++ ini5.py
@@ -0,0 +1,13 @@
+"""
+    This program receive a file with n lines and return a new file containing all the even-numbered lines
+     from the original file. Assume 1-based numbering of lines.
+"""
+
+f = open('rosalind_ini5.txt', 'r')
+s = open('newfile.txt', 'w')
+index = 0
+
+for i in f:
+    if index % 2 == 1:
+        s.write(str(i))
+    index += 1
